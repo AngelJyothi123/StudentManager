278205aab51ace319f73433555b9104c76a376ac
AngelJyothi123 <cherukuriangeljyothi@gmail.com>
2026-02-26 14:38:37 +0530

    firstly we modified the code

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        Student student1 = new Student(1, "Angel");
+        Student student2 = new Student(2, "Jyothi");
+
+        System.out.println(student1);
+        System.out.println(student2);
+    }
+}
